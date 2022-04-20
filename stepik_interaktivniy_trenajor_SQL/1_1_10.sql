7e46c464b7c2584a4b9c2615b4fa992e6546b63c
“Muzyria” <fila090580@gmail.com>
2022-04-20 19:49:56 +0300

    new

---
+++ stepik_interaktivniy_trenajor_SQL/1_1_10.sql
@@ -0,0 +1,6 @@
+INSERT INTO book (title, author, price, amount)
+VALUE ('Белая гвардия',	'Булгаков М.А.', 540.50, 5);
+INSERT INTO book (title, author, price, amount)
+VALUE ('Идиот',	'Достоевский Ф.М.',	460.00,	10);
+INSERT INTO book (title, author, price, amount)
+VALUE ('Братья Карамазовы',	'Достоевский Ф.М.',	799.01,	2);
