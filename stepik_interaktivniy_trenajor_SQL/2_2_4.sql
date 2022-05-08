39f2b58e0adbaa372056eff10527d5a285d97ddf
“Muzyria” <fila090580@gmail.com>
2022-05-09 00:32:57 +0300

    new

---
+++ stepik_interaktivniy_trenajor_SQL/2_2_4.sql
@@ -0,0 +1,8 @@
+SELECT
+    name_city,
+    name_author,
+    DATE_ADD('2020-01-01', INTERVAL FLOOR(RAND()*365) DAY) AS Дата
+FROM city CROSS JOIN author
+ORDER BY
+    name_city ASC,
+    Дата DESC;
